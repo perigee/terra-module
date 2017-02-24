f23471c334f72691f12948d5e8ce54771e47416c
Jun HU <jun.hu@ubisoft.com>
2017-02-24 20:13:29 +0000

    init volume

---
+++ openstack/volume/main.tf
@@ -0,0 +1,18 @@
+
+variable "vol_name" {}
+variable "vol_description" { default = ""}
+variable "vol_size" {}
+variable "vol_tags" {}
+variable "vol_type" {default = "san_high_perf"}
+
+resource "openstack_blockstorage_volume_v2" "volume" {
+	 name = "${var.vol_name}"
+	 size = "${var.vol_size}"
+	 volume_type = "${var.vol_type}"
+	 metadata = ${var.vol_tags}"
+}
+
+
+output "vol_id" {
+       value = "openstack_blockstorage_volume_v2.volume.id"
+}
