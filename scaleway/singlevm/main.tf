d1eeb03ee3824664c27de11b3d781887e7f880a7
Jun HU <junhufr@gmail.com>
2017-06-27 15:20:09 -0400

    set dynamic ip

--- scaleway/singlevm/main.tf
+++ scaleway/singlevm/main.tf
@@ -11,19 +11,15 @@ variable "vm_sg" {
   default = "500caed3-3d35-42cc-930b-11447ee64bde"
 }
 
-resource "scaleway_server" "test" {
-  name           = "${var.vm_name == "testing" ? format("%s_%s", var.vm_name, timestamp()) : var.vm_name}"
-  image          = "89ee4018-f8c3-4dc4-a6b5-bca14f985ebe"
-  type           = "VC1S"
-  tags           = "${var.vm_tags}"
-  security_group = "${var.vm_sg}"
-}
-
-resource "scaleway_ip" "test_ip" {
-  depends_on = ["scaleway_server.test"]
-  server     = "${scaleway_server.test.id}"
+resource "scaleway_server" "single" {
+  name                = "${var.vm_name == "testing" ? format("%s_%s", var.vm_name, timestamp()) : var.vm_name}"
+  image               = "89ee4018-f8c3-4dc4-a6b5-bca14f985ebe"
+  type                = "VC1S"
+  tags                = "${var.vm_tags}"
+  security_group      = "${var.vm_sg}"
+  dynamic_ip_required = true
 }
 
 output "ipv4" {
-  value = "${scaleway_ip.test_ip.ip}"
+  value = "${scaleway_server.single.public_ip}"
 }
